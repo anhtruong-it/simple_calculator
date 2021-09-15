4c95178b2d0d4d8553013a38615a7f0c21021180
Anh Truong Nguyen <87184721+anhtruong-it@users.noreply.github.com>
2021-09-15 21:10:48 +1000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,55 @@
 import wx
 
-import wx.grid
-
-
 class Calulator(wx.Frame):
     def __init__(self, parent, id,  title):
         wx.Frame.__init__(self, parent, id, title, size=(600, 400))
+        self.initialise()
 
-        self.cal()
-
-    def cal(self):
-        self.Show(True)
+    def initialise(self):
+        # create title
         self.pnl = wx.Panel(self, size=(600, 400))
-        self.number1 = wx.TextCtrl(self.pnl, pos=(1,5))
-        self.number2 = wx.TextCtrl(self.pnl, pos=(1,30))
-        self.answer= wx.TextCtrl(self.pnl, pos=(1,60))
+        self.row = wx.BoxSizer(wx.VERTICAL)
+        self.title = wx.StaticText(self.pnl, label="Simple Calculator")
+        self.font = self.title.GetFont()
+        self.font = self.font.Bold()
+        self.title.SetFont((self.font))
+        self.row.Add(self.title, 1, wx.ALIGN_CENTER|wx.BOTTOM, border=2)
 
+        # create two inputs
+        self.number = wx.BoxSizer(wx.HORIZONTAL)
+        self.number1 = wx.TextCtrl(self.pnl)
+        self.number2 = wx.TextCtrl(self.pnl)
+        self.number.Add(self.number1, 1, wx.ALIGN_CENTER|wx.RIGHT, border=5)
+        self.number.Add(self.number2, 1, wx.ALIGN_CENTER|wx.RIGHT, border=5)
+        self.row.Add(self.number, 2, wx.ALIGN_CENTER)
+        self.SetSizerAndFit(self.row)
+
+        # create a answer title
+        self.title = wx.StaticText(self.pnl, label="Answer")
+        self.font = self.title.GetFont()
+        self.font = self.font.Bold()
+        self.title.SetFont((self.font))
+        self.row.Add(self.title, 1, wx.ALIGN_CENTER | wx.BOTTOM, border=2)
+
+        # create answer box
+        self.answer = wx.StaticText(self.pnl, label=" ")
+        self.answer.SetBackgroundColour(wx.Colour(255, 255, 255, 255))
+        self.answer.SetMinSize((100, 50))
+        self.row.Add(self.answer, 1, wx.ALIGN_CENTER|wx.BOTTOM, border=10)
+
+        # create 4 buttons
+        self.button = wx.BoxSizer(wx.HORIZONTAL)
         self.theButtonPlus = wx.Button(self.pnl, label="+")
         self.theButtonMinus = wx.Button(self.pnl, label="-")
         self.theButtonTime = wx.Button(self.pnl, label="x")
         self.theButtonDivide = wx.Button(self.pnl, label="/")
         self.theButtonClear = wx.Button(self.pnl, label="clear")
+        self.button.Add(self.theButtonPlus, 1, wx.ALIGN_CENTER|wx.BOTTOM, border=10)
+        self.button.Add(self.theButtonMinus, 1, wx.ALIGN_CENTER | wx.BOTTOM, border=10)
+        self.button.Add(self.theButtonTime, 1, wx.ALIGN_CENTER | wx.BOTTOM, border=10)
+        self.button.Add(self.theButtonDivide, 1, wx.ALIGN_CENTER | wx.BOTTOM, border=10)
+        self.button.Add(self.theButtonClear, 1, wx.ALIGN_CENTER | wx.BOTTOM, border=10)
+        self.row.Add(self.button, 1, wx.ALIGN_CENTER)
 
         self.Bind(wx.EVT_BUTTON, self.buttonPressAdd, self.theButtonPlus)
         self.Bind(wx.EVT_BUTTON, self.buttonPressMinus, self.theButtonMinus)
@@ -28,57 +57,76 @@ class Calulator(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.buttonPressDivide, self.theButtonDivide)
         self.Bind(wx.EVT_BUTTON, self.buttonPressClear, self.theButtonClear)
 
-        self.sizer = wx.GridBagSizer(hgap=200, vgap=40)
-
-        self.sizer.Add(self.theButtonPlus, pos=(1, 1), flag=wx.ALIGN_CENTER | wx.ALL, border=5)
-        self.sizer.Add(self.theButtonMinus, pos=(2, 1), flag=wx.ALIGN_CENTER | wx.ALL, border=2)
-        self.sizer.Add(self.theButtonTime, pos=(3, 1), flag=wx.ALIGN_CENTER | wx.ALL, border=2)
-        self.sizer.Add(self.theButtonDivide, pos=(4, 1), flag=wx.ALIGN_CENTER | wx.ALL, border=2)
-        self.sizer.Add(self.theButtonClear, pos=(5, 1), flag=wx.ALIGN_CENTER | wx.ALL, border=2)
-        self.pnl.SetSizerAndFit(self.sizer)
-
-    #def getValues(self):
-
+        self.pnl.SetSizerAndFit(self.row)
+        self.Show(True)
 
     def buttonPressAdd(self, event):
-        int1 = int(self.number1.Value)
-        int2 = int(self.number2.Value)
-        self.result = int1+int2
-        self.answer.display= self.result
-        self.number1.Value = ""
-        self.number2.Value = ""
+        try:
+            num1 = int(self.number1.Value)
+            num2 = int(self.number2.Value)
+            self.result = num1 + num2
+            self.answer.Label = str(self.result)
+            self.number1.Value = ""
+            self.number2.Value = ""
 
-
+        except ValueError:
+            wx.MessageBox("Enter 2 numbers")
+            self.number1.Value = ""
+            self.number2.Value = ""
+            self.answer.Label = ""
 
     def buttonPressMinus(self, event):
-        int1 = int(self.number1.Value)
-        int2 = int(self.number2.Value)
+        try:
+            num1 = int(self.number1.Value)
+            num2 = int(self.number2.Value)
+            self.result = num1 - num2
+            self.answer.Label = str(self.result)
+            self.number1.Value = ""
+            self.number2.Value = ""
 
-
-
-
-
-        return int1+int2
+        except ValueError:
+            wx.MessageBox("Enter 2 numbers")
+            self.number1.Value = ""
+            self.number2.Value = ""
+            self.answer.Label = ""
 
     def buttonPressTime(self, event):
-        int1 = int(self.number1.Value)
-        int2 = int(self.number2.Value)
+        try:
+            num1 = int(self.number1.Value)
+            num2 = int(self.number2.Value)
+            self.result = num1 * num2
+            self.answer.Label = str(self.result)
+            self.number1.Value = ""
+            self.number2.Value = ""
 
-        return int1+int2
+        except ValueError:
+            wx.MessageBox("Enter 2 numbers")
+            self.number1.Value = ""
+            self.number2.Value = ""
+            self.answer.Label = ""
 
     def buttonPressDivide(self, event):
-        int1 = int(self.number1.Value)
-        int2 = int(self.number2.Value)
-        return int1+int2
+        try:
+            num1 = int(self.number1.Value)
+            num2 = int(self.number2.Value)
+            self.result = num1 / num2
+            self.answer.Label = str(self.result)
+            self.number1.Value = ""
+            self.number2.Value = ""
+
+        except ValueError:
+            wx.MessageBox("Enter 2 numbers")
+            self.number1.Value = ""
+            self.number2.Value = ""
+            self.answer.Label = ""
 
     def buttonPressClear(self, event):
-        int1 = int(self.number1.Value)
-        int2 = int(self.number2.Value)
-        return int1+int2
-
+        self.number1.Value = ""
+        self.number2.Value = ""
+        self.answer.Label = ""
 
 
 app = wx.App()
-frame = Calulator(None,-1,'My App')
-#frame = GridFrame(None)
+frame = Calulator(None, -1,'My Calculator')
+
 app.MainLoop()
